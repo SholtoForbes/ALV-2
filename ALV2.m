5efb0179ecfb7edbed36e2b2579870e10c0010cd
SholtoForbes <sholtoforbes@hotmail.com>
2016-03-02 10:45:44 +1000

    made small changes for 0.1.0 program distr

--- ALV2.m
+++ ALV2.m
@@ -2,6 +2,30 @@
 % Sholto Forbes-Spyratos
 clear all
 
+h = msgbox('ALV-2 Trajectory Simulation is Running');
+
+
+% Initial Conditions
+% r0 = 0; % Altitude (m)
+% xi0 = deg2rad(153); % Longitude (rad)
+% phi0 = deg2rad(-27); % Latitude (rad)
+% gamma0 = deg2rad(90); % Flight Path Angle (rad)
+% zeta0 = deg2rad(97); % Heading Angle (rad)
+
+prompt = {'Launch Altitude (km)','Launch Longitude (deg)','Launch Latitude (deg)', 'Launch Angle (deg)', 'Launch Heading Angle (deg)', 'Target Altitude (km)'};
+dlg_title = 'Inputs';
+num_lines = 1;
+defaultans = {'0','153','-27','90','97', '400'};
+answer = inputdlg(prompt,dlg_title,num_lines,defaultans);
+
+% Initial Conditions
+r0 = str2num(answer{1})*1000; % Altitude (m)
+xi0 = deg2rad(str2num(answer{2})); % Longitude (rad)
+phi0 = deg2rad(str2num(answer{3})); % Latitude (rad)
+gamma0 = deg2rad(str2num(answer{4})); % Flight Path Angle (rad)
+zeta0 = deg2rad(str2num(answer{5})); % Heading Angle (rad)
+rTarget = str2num(answer{6})*1000; % Target Altitude (m)
+
 % optimisation parameters
 A = [];
 b = [];
@@ -17,41 +41,46 @@ x0 = [0,0];
 nonlcon = [];
 options = optimoptions('fmincon','Display','iter','Algorithm','sqp','UseParallel',true);
 
-x = fmincon(@ALV2FUNCTION,x0,A,b,Aeq,beq,lb,ub,nonlcon, options)  % run trajectory calculation routine, this determines the correct angle of attack schedule for the upper stages
+x = fmincon(@(x)ALV2FUNCTION(x,r0,gamma0,xi0,phi0,zeta0,rTarget),x0,A,b,Aeq,beq,lb,ub,nonlcon, options)  % run trajectory calculation routine, this determines the correct angle of attack schedule for the upper stages
 
 
-[rdiff,t,r,gamma,v,m,xi,phi,zeta,i12,i23] = ALV2FUNCTION(x); % simulate trajectory
+[rdiff,t,r,gamma,v,m,xi,phi,zeta,i12,i23,alpha] = ALV2FUNCTION(x,r0,gamma0,xi0,phi0,zeta0,rTarget); % simulate trajectory
 
 r_E = 6371000; % radius of Earth (m)
 
 figure(1)
 
-subplot(4,1,1)
+subplot(5,1,1)
 hold on
 plot(t(1:i12),(r(1:i12)-r_E)/1000,'LineWidth',1.5,'Color','b')
 plot(t(i12:i23),(r(i12:i23)-r_E)/1000,'LineWidth',1.5,'Color','r')
 plot(t(i23:end),(r(i23:end)-r_E)/1000,'LineWidth',1.5,'Color','g')
 ylabel('Altitude (km)');
-subplot(4,1,2)
+subplot(5,1,2)
 hold on
 plot(t(1:i12),rad2deg(gamma(1:i12)),'LineWidth',1.5,'Color','b')
 plot(t(i12:i23),rad2deg(gamma(i12:i23)),'LineWidth',1.5,'Color','r')
 plot(t(i23:end),rad2deg(gamma(i23:end)),'LineWidth',1.5,'Color','g')
 ylabel('Trajectory Angle (deg)');
-subplot(4,1,3)
+subplot(5,1,3)
 hold on
 plot(t(1:i12),v(1:i12),'LineWidth',1.5,'Color','b')
 plot(t(i12:i23),v(i12:i23),'LineWidth',1.5,'Color','r')
 plot(t(i23:end),v(i23:end),'LineWidth',1.5,'Color','g')
 ylabel('Velocity (m/s)');
-subplot(4,1,4)
+subplot(5,1,4)
 hold on
 plot(t(1:i12),m(1:i12),'LineWidth',1.5,'Color','b')
 plot(t(i12:i23),m(i12:i23),'LineWidth',1.5,'Color','r')
 plot(t(i23:end),m(i23:end),'LineWidth',1.5,'Color','g')
 ylabel('Mass (kg)');
+subplot(5,1,5)
+hold on
+plot(t(1:i12),rad2deg(alpha(1:i12)),'LineWidth',1.5,'Color','b')
+plot(t(i12:i23),rad2deg(alpha(i12:i23)),'LineWidth',1.5,'Color','r')
+plot(t(i23:end),rad2deg(alpha(i23:end)),'LineWidth',1.5,'Color','g')
+ylabel('Angle of Attack (deg)');
 xlabel('Time (s)');
-
 
 figure(2)
 hold on
@@ -61,22 +90,27 @@ geoshow(rad2deg(phi(i12:i23)),rad2deg(xi(i12:i23)),'LineWidth',2,'Color','r')
 geoshow(rad2deg(phi(i23:end)),rad2deg(xi(i23:end)),'LineWidth',2,'Color','g')
 
 
-% =========================================================================
+%==========================================================================
 % ---------------------------    Flyback    -------------------------------
 %==========================================================================
 
+% % lb = [-0.1, -45.]; % lower bounds of A, B
+% % ub = [0.1,45.]; % upper bounds of A, B
+% % 
+% % x0 = [0,0];
+% 
+% % x = fmincon(@(x)ALV2Flyback(x, t(i12),r(i12),gamma(i12),v(i12),xi(i12),phi(i12),zeta(i12)),x0,A,b,Aeq,beq,lb,ub,nonlcon, options)
 
-
-[t_fb,r_fb,gamma_fb,v_fb,m_fb,xi_fb,phi_fb,zeta_fb] = ALV2Flyback(t(i12),r(i12),gamma(i12),v(i12),xi(i12),phi(i12),zeta(i12));
-
-
-figure(3)
-subplot(4,1,1)
-plot(t_fb,(r_fb-r_E)/1000,'LineWidth',1.5,'Color','b')
-subplot(4,1,2)
-plot(t_fb,rad2deg(gamma_fb),'LineWidth',1.5,'Color','b')
-subplot(4,1,3)
-plot(t_fb,v_fb,'LineWidth',1.5,'Color','b')
+% [rdiff,t_fb,r_fb,gamma_fb,v_fb,m_fb,xi_fb,phi_fb,zeta_fb] = ALV2Flyback(45, t(i12),r(i12),gamma(i12),v(i12),xi(i12),phi(i12),zeta(i12));
+% 
+% 
+% figure(3)
+% subplot(4,1,1)
+% plot(t_fb,(r_fb-r_E)/1000,'LineWidth',1.5,'Color','b')
+% subplot(4,1,2)
+% plot(t_fb,rad2deg(gamma_fb),'LineWidth',1.5,'Color','b')
+% subplot(4,1,3)
+% plot(t_fb,v_fb,'LineWidth',1.5,'Color','b')
 
 
 
--- ALV2Flyback.m
+++ ALV2Flyback.m
@@ -1,11 +1,13 @@
-function [t,r,gamma,v,m,xi,phi,zeta] = ALV2Flyback(t0,r0,gamma0,v0,xi0,phi0,zeta0)
+function [rdiff,t,r,gamma,v,m,xi,phi,zeta] = ALV2Flyback(x,t0,r0,gamma0,v0,xi0,phi0,zeta0)
 % Flyback Simulation of the ALV-2 First Stages
 % Sholto Forbes-Spyratos
 
+
 % Set Conditions
+
 r_E = 6371000; % radius of Earth (m)
 
-dt = 0.1; % Timestep (s)
+dt = 1.; % Timestep (s)
 
 % Initialise Position Arrays
 t(1) = t0;
@@ -17,23 +19,45 @@ phi(1) = phi0;
 zeta(1) = zeta0;
 
 m = 480; % Mass (kg)
-alpha = deg2rad(30);
 A = 0.5; % Reference Area (m^2)
+Al = 2; % Lift Reference Area (m^2) THIS IS AN ASSUMPTION
 
 i = 1;
 
 T = 0; % Thrust
 
-while v > 500
+t_temp(1) = 0; % initiate temporary time scale for alpha calculation
+
+alpha = 0;
+
+j = 1;
+
+while r(i) > r_E + 20000
+
+if i > 2
+if r(i) < r(i-1)
+% Calculate Alpha
+alpha = deg2rad(x);
+t_temp(j+1) = t_temp(j) + dt;
+j = j+1;
+end    
+end
+
+% alpha = deg2rad(x);
+    
 % Calculate Aerodynamics
 atmosphere = dlmread('atmosphere.txt');
 
+if r(i) - r_E < 85000
 rho = interp1(atmosphere(:,1),atmosphere(:,4),r(1)-r_E);
+else
+rho = 0;
+end
 
 Cl = 1.1*sin(alpha)^2*cos(alpha);
-Cd = 1.1*sin(alpha)^3 + 0.01;
+Cd = 1.1*sin(alpha)^3 + 0.2;
 
-L(i) = 0.5* Cl * v(i)^2 * A * rho;
+L(i) = 0.5* Cl * v(i)^2 * Al * rho;
 D(i) = 0.5* Cd * v(i)^2 * A * rho;
     
 % Increment Equations of Motion
@@ -51,6 +75,9 @@ zeta(i+1) = zeta(i) + dt*zetadot;
 i = i+1;
 end
 
+rdiff = v(end) ; % Function to be minimised. Controls the target altitude and flight path angle.
 
+disp('Convergence Parameter') ;
+disp(rdiff);
 end
 
